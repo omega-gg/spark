bf04e02fd0ee6081d81204d0ab228904c5eded1e
Benjamin Arnaud <bunjee@omega.gg>
2026-01-28 12:00:49 +0100

    Create build.sh (9)

--- build.sh
+++ build.sh
@@ -70,9 +70,9 @@ sudo apt-get install -y git python3 python3-pip python3-venv nodejs
 if ! command -v rustup >/dev/null 2>&1; then
 
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-    source "$HOME/.cargo/env"
 fi
+
+. "$HOME/.cargo/env"
 
 rustup target add wasm32-unknown-unknown
 
